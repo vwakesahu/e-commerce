ce9867f8cc4a418581a7026dcfc6e44175bc8f8a
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-08-05 13:17:53 +0530

    Added some animations and background img to Menu items

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -3,9 +3,10 @@ import "./menu-item.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div className={`${size} menu-items`} style={{ backgroundImage: `url(${imageUrl})` }}>
+    <div className={`${size} menu-items`} >
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="content">
-        <h1>{title}</h1>
+        <h1>{title.toUpperCase()}</h1>
         <span>SHOP NOW</span>
       </div>
     </div>
